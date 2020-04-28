8ec653a51c12499739066e865c2bdddbfdbf7205
Fatai Balogun <fattylee.remod@gmail.com>
2020-04-28 14:19:28 +0100

    complete form validation

--- js/main.js
+++ js/main.js
@@ -1,24 +1,87 @@
 const form = document.querySelector("#form");
-// const username = document.querySelector("#username");
-// const email = document.querySelector("#email");
-// const password = document.querySelector("#password");
-// const password2 = document.querySelector("#password2");
-
 const { username, email, password, password2, submit } = form.elements;
-form.addEventListener("submit", (event) => {
+
+form.addEventListener("submit", handleFormValidation);
+
+function handleFormValidation(event) {
   event.preventDefault();
-  const usernameValue = username.value;
+  const usernameValue = username.value.trim();
   const emailValue = email.value.trim();
   const passwordValue = password.value;
   const password2Value = password2.value;
+  isValid = false;
 
-  clearAllFields();
-  console.log(usernameValue, emailValue, passwordValue, password2Value);
-});
+  // validate username field
+  if (usernameValue === "") {
+    addErrorFlag(username, "Username cannot be blank");
+  } else if (usernameValue.length < 3) {
+    addErrorFlag(username, "Username length cannot be less than 3 characters");
+  } else {
+    removeErrorFlag(username);
+  }
+  // validate email field
+  if (emailValue === "") {
+    addErrorFlag(email, "Email cannot be blank");
+  } else if (emailValue.length < 10) {
+    addErrorFlag(email, "Email length cannot be less than 10 characters");
+  } else {
+    removeErrorFlag(email);
+  }
 
+  // validate password field
+  if (passwordValue === "") {
+    addErrorFlag(password, "Password cannot be blank");
+  } else if (passwordValue.length < 6) {
+    addErrorFlag(
+      password,
+      "Password length cannot be less than 6 characters long"
+    );
+  } else {
+    removeErrorFlag(password);
+  }
+  // validate password check field
+  if (password2Value === "") {
+    addErrorFlag(password2, "Password check cannot be blank");
+  } else if (passwordValue !== password2Value) {
+    addErrorFlag(password2, 'Password check does not match "Password"');
+  } else {
+    removeErrorFlag(password2);
+  }
+
+  if (isValid) {
+    clearAllFields();
+    restoreFormState();
+    showModal();
+  }
+} // End handleFormValidation
+
+document.querySelector(".modal a").addEventListener("click", showForm);
+
+function showForm() {
+  form.parentElement.className = "container";
+  document.querySelector(".modal").className = "modal";
+}
+function showModal() {
+  form.parentElement.className = "container hide";
+  document.querySelector(".modal").className = "modal show";
+}
+function addErrorFlag(field, errorMsg) {
+  field.parentElement.className = "form-control error";
+  field.parentElement.querySelector("small").innerHTML = errorMsg;
+  isValid = false;
+}
+function removeErrorFlag(field) {
+  field.parentElement.className = "form-control success";
+  isValid = true;
+}
 function clearAllFields() {
   username.value = "";
   email.value = "";
   password.value = "";
   password2.value = "";
+}
+function restoreFormState() {
+  Array.from(form.elements).forEach((e) => {
+    e.parentElement.className = "form-control";
+  });
 }
