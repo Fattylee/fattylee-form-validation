92b7e9e620b8feb6f493440ee455c2da7d6b9465
Fatai Balogun <fattylee.remod@gmail.com>
2020-05-11 21:08:44 +0100

    populate table with a dummy data

--- js/table.js
+++ js/table.js
@@ -1,1 +1,21 @@
-alert("somethinh is fishy");
+const data = [
+  { name: "abu adnaan", age: 32, birthday: "23/06/1987" },
+  { name: "john doe", age: 35, birthday: "13/02/1984" },
+  { name: "james smith", age: 20, birthday: "3/09/1999" },
+];
+buildTable(data);
+function buildTable(data) {
+  const dynamicTable = document.querySelector(".dynamic-table");
+  const tbody = dynamicTable.querySelector("tbody");
+  tbody.innerHTML = "";
+  data.forEach(({ name, age, birthday }) => {
+    const template = `
+<tr>
+  <td>${name}</td>
+  <td>${age}</td>
+  <td>${birthday}</td>
+</tr>
+`;
+    tbody.innerHTML += template;
+  });
+}
