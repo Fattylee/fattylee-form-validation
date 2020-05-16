c4afe83676e1a46d6141bc79590b4d2f21992349
Fatai Balogun <fattylee.remod@gmail.com>
2020-05-16 17:16:24 +0100

    add sortby to table

--- js/table.js
+++ js/table.js
@@ -3,6 +3,7 @@ const data = [
   { name: "john doe", age: 35, birthday: "13/02/1984" },
   { name: "james smith", age: 20, birthday: "3/09/1999" },
 ];
+
 buildTable(data);
 function buildTable(data) {
   const dynamicTable = document.querySelector(".dynamic-table");
@@ -19,3 +20,51 @@ function buildTable(data) {
     tbody.innerHTML += template;
   });
 }
+
+(function sortArray(arr) {
+  // add event handlers to table headers
+  document.querySelectorAll(".dynamic-table th").forEach((th) => {
+    th.addEventListener("click", handleSort);
+  });
+
+  function handleSort(event) {
+    const col = event.target.getAttribute("data-col");
+    const sort = event.target.getAttribute("data-sort");
+    const caret = event.target.querySelector("i");
+    console.log(caret);
+    console.log(event.target);
+    if (col == "name") {
+      if (sort == "asc") {
+        event.target.setAttribute("data-sort", "desc");
+        arr.sort((a, b) => (a.name > b.name ? -1 : 1));
+        caret.className = "fas fa-caret-down";
+      } else {
+        event.target.setAttribute("data-sort", "asc");
+        arr.sort((a, b) => (a.name < b.name ? -1 : 1));
+        caret.className = "fas fa-caret-up";
+      }
+    } else if (col == "age") {
+      if (sort == "asc") {
+        event.target.setAttribute("data-sort", "desc");
+        arr.sort((a, b) => (a.age > b.age ? -1 : 1));
+        caret.className = "fas fa-caret-down";
+      } else {
+        event.target.setAttribute("data-sort", "asc");
+        arr.sort((a, b) => (a.age < b.age ? -1 : 1));
+        caret.className = "fas fa-caret-up";
+      }
+    } else {
+      if (sort == "asc") {
+        event.target.setAttribute("data-sort", "desc");
+        arr.sort((a, b) => (a.birthday > b.birthday ? -1 : 1));
+        caret.className = "fas fa-caret-down";
+      } else {
+        event.target.setAttribute("data-sort", "asc");
+        arr.sort((a, b) => (a.birthday < b.birthday ? -1 : 1));
+        caret.className = "fas fa-caret-up";
+      }
+    }
+
+    buildTable(arr);
+  }
+})(data);
